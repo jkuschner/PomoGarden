ab6fadd610c7177aac4ea94dc7bc0d0c368f1069
Chi Chow <cschow@ucsd.edu>
2021-02-09 00:59:55 -0800

    Changed sum function

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,5 +1,5 @@
 const sum = require('sum')
 
-test('Sum 2 + 1 equal 3', function () {
-    expect(sum(2, 1)).toBe(3)
+test('Sum 2 + 1 + 3 equal 6', function () {
+    expect(sum(2, 1, 3)).toBe(6)
 })
