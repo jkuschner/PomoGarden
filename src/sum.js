30b19507ab5d86177619db921e1e651b78abb976
DanielwasTaken <53798206+DanielwasTaken@users.noreply.github.com>
2021-02-28 16:53:47 -0800

    Add files via upload

---
+++ src/sum.js
@@ -0,0 +1,11 @@
+/**
+ * Adds 3 numbers...
+ * @param {Number} a First number
+ * @param {Number} b Second number
+ * @param {Number} c Third number
+ */
+function sum(a, b, c) {
+    return a + b + c
+}
+
+module.exports = sum
