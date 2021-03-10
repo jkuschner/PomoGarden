6cfc583834c46f9e2254160a5d5ab6b0839991f6
Chi Chow <cschow@ucsd.edu>
2021-03-10 01:21:36 -0800

    Cache first approach, switch between cache/no cache in dev

--- src/pomo-sw.js
+++ src/pomo-sw.js
@@ -59,15 +59,16 @@ self.addEventListener('activate', (e) => {
 self.addEventListener('fetch', (e) => {
     e.respondWith(
         (async () => {
-            try {
-                const networkRes = await fetch(e.request)
-                const cache = await caches.open(cacheName)
-                cache.put(e.request, networkRes.clone())
-                return networkRes
-            } catch (error) {
-                const cachedRes = await caches.match(e.request)
-                return cachedRes
+            const r = await caches.match(e.request)
+            console.log(`[Service Worker] Fetching resource: ${e.request.url}`)
+            if (r) {
+                return r
             }
+            const response = await fetch(e.request)
+            const cache = await caches.open(cacheName)
+            console.log(`[Service Worker] Caching new resource: ${e.request.url}`)
+            cache.put(e.request, response.clone())
+            return response
         })()
     )
 })
